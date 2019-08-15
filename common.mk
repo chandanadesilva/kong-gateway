8e63b686fbd463730b501544008690f8d843d89d
Chandana De Silva <chandanads@swann.com>
2019-08-16 09:24:55 +1000

    initail commit

---
+++ common.mk
@@ -0,0 +1,9 @@
+AWS_ACCOUNT:=$(shell aws iam list-roles --query 'Roles[?RoleName==`AWSServiceRoleForTrustedAdvisor`].[Arn]' --output text | cut -d \: -f 5)
+# stack templates
+STACK_TEMPLATE_VPC:=cfn/vpc.yml
+STACK_TEMPLATE_GATEWAY_ECR:=cfn/ecr-gateway.yml
+STACK_TEMPLATE_ECS_CLUSTER:=cfn/ecs-cluster.yml
+
+STACK_NAME_VPC := $(basename $(notdir $(STACK_TEMPLATE_VPC)))
+STACK_NAME_GATEWAY_ECR := $(basename $(notdir $(STACK_TEMPLATE_GATEWAY_ECR)))
+STACK_NAME_ECS_CLUSTER := $(basename $(notdir $(STACK_TEMPLATE_ECS_CLUSTER)))
